65efe4507fcf3e2cfb9e7a2da3d11fa42b417461
Filip Vrba <filipvrba@pm.me>
2022-11-02 21:29:05 +0100

    generate function

--- src/js/app/main.js
+++ src/js/app/main.js
@@ -1,6 +1,5 @@
-// import path, from: "path"
-// import fs, from: "fs"
 import { options } from "./arguments.js";
 import * as Ssg from "../lib/suitescript_generator.js";
-Ssg.generate;
-console.log(Ssg)
+
+// TODO: add interactive console
+Ssg.generate(options)
--- src/js/lib/suitescript_generator.js
+++ src/js/lib/suitescript_generator.js
@@ -2,6 +2,6 @@ import { VERSION } from "./suitescript_generator/version.js";
 import * as Helper from "./suitescript_generator/helper.js";
 import Config from "./suitescript_generator/config.js";
 
-function generate() {
+function generate(options) {
   let config = new Config
 }
